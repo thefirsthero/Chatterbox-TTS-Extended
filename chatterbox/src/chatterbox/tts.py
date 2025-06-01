6007ced93a0c1c6ed86a83ecce7971f71d3e3454
petermg <pgomberg@gmail.com>
2025-05-31 19:04:30 -0700

    Update tts.py

--- chatterbox/src/chatterbox/tts.py
+++ chatterbox/src/chatterbox/tts.py
@@ -195,6 +195,7 @@ class ChatterboxTTS:
         exaggeration=0.5,
         cfg_weight=0.5,
         temperature=0.8,
+        apply_watermark=True,
     ):
         if audio_prompt_path:
             self.prepare_conditionals(audio_prompt_path, exaggeration=exaggeration)
@@ -240,5 +241,7 @@ class ChatterboxTTS:
                 ref_dict=self.conds.gen,
             )
             wav = wav.squeeze(0).detach().cpu().numpy()
-            watermarked_wav = self.watermarker.apply_watermark(wav, sample_rate=self.sr)
-        return torch.from_numpy(watermarked_wav).unsqueeze(0)
+            if apply_watermark:
+                wav = self.watermarker.apply_watermark(wav, sample_rate=self.sr)
+            return torch.from_numpy(wav).unsqueeze(0)
+            
